14894bf9dce561f8a64f09923f6c61a4616d2f41
Evgeniy Safonov <rising.niar@gmail.com>
2018-04-06 17:32:02 +0300

    Домашнее задание 5

--- src/main/java/ru/sbt/jschool/session5/problem1/SQLGenerator.java
+++ src/main/java/ru/sbt/jschool/session5/problem1/SQLGenerator.java
@@ -1,8 +1,6 @@
 package ru.sbt.jschool.session5.problem1;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
